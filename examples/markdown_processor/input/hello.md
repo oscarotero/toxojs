7a88ca3456399a00f8c6bde0f994aa550cb58a09
Oscar Otero <oom@oscarotero.com>
2025-07-12 13:16:34 +0200

    example rendering markdown

---
+++ examples/markdown_processor/input/hello.md
@@ -0,0 +1,4 @@
+# Hello world
+
+This is a simple example of a Markdown file. You can add **bold text**, _italic
+text_, or even [links](https://www.example.com).
---
+++ examples/markdown_processor/main.js
@@ -0,0 +1,23 @@
+import "https://cdn.jsdelivr.net/npm/markdown-it@14.1.0/dist/markdown-it.min.js";
+
+const md = markdownit();
+
+const folder = await navigator.storage.getDirectory();
+const input = await folder.getDirectoryHandle("input");
+const output = await folder.getDirectoryHandle("output", { create: true });
+
+// Iterate through all files in the input directory
+for await (const entry of input.values()) {
+  if (entry.kind === "file" && entry.name.endsWith(".md")) {
+    const file = await entry.getFile();
+    const text = await file.text();
+
+    const processedText = md.render(text);
+
+    const filename = entry.name.replace(".md", ".html");
+    const out = await output.getFileHandle(filename, { create: true });
+    const writable = await out.createWritable();
+    await writable.write(processedText);
+    await writable.close();
+  }
+}
